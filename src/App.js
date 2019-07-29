69eeb03b2943523701bc89e312a4f3607978c7da
Minh Lam Su <kelvincoder@gmail.com>
2019-07-29 14:10:59 +0700

    Update locale vietnamese

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import users from './components/users';
-import places from './components/places';
-import objects from './components/objects';
-import languages from './components/languages';
-import dashboard from './components/dashboard';
-import authProvider from './providers/authProvider';
-//import dataProvider from './providers/dataProvider';
-import myDataProvider from './providers/myDataProvider';
-import server from 'ra-data-json-server'
-import { Admin, Resource } from 'react-admin';
+import React from "react";
+import users from "./components/users";
+import places from "./components/places";
+import objects from "./components/objects";
+import languages from "./components/languages";
+import dashboard from "./components/dashboard";
+import authProvider from "./providers/authProvider";
+import myDataProvider from "./providers/myDataProvider";
+import { Admin, Resource } from "react-admin";
 import { AppConstant } from "./providers/constants";
-import './App.css';
-
+import vietnameseMessages from "ra-language-vietnamese";
+import "./App.css";
 
 const dataProvider = myDataProvider(AppConstant.API_URL);
-//const dataProvider = server('http://localhost:8000')
-const displayLabel = name => ({ label: name })
+const displayLabel = name => ({ label: name });
+const messages = {
+  vi: vietnameseMessages
+};
+
+const i18nProvider = locale => messages[locale];
+
 const App = () => (
-  <Admin dashboard={dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource name="users-admin" options={displayLabel('Users')} {...users} />
-    <Resource name="places-admin" options={displayLabel('Places')} {...places} />
-    <Resource name="objects" options={displayLabel('Objects')} {...objects} />
-    <Resource name="languages" options={displayLabel('Languages')} {...languages} />
+  <Admin
+    locale="vi"
+    i18nProvider={i18nProvider}
+    dashboard={dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource name="users-admin" options={displayLabel("Users")} {...users} />
+    <Resource
+      name="places-admin"
+      options={displayLabel("Places")}
+      {...places}
+    />
+    <Resource name="objects" options={displayLabel("Objects")} {...objects} />
+    <Resource
+      name="languages"
+      options={displayLabel("Languages")}
+      {...languages}
+    />
   </Admin>
-)
+);
 
 export default App;
