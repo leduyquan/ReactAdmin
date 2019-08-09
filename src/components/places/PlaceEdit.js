aa8b3464d4408ef15a5999d3b81ed023c6200d3b
Minh Lam Su <kelvincoder@gmail.com>
2019-08-09 13:59:57 +0700

    Fix the bug location

--- src/components/places/PlaceEdit.js
+++ src/components/places/PlaceEdit.js
@@ -316,15 +316,14 @@ class PlaceEdit extends Component {
             />
             <FormDataConsumer>
               {({ formData, scopedFormData, getSource, dispatch, ...rest }) => {
-                console.log("formData", formData);
                 if (
                   formData.location === undefined ||
                   formData.location === null
                 ) {
-                  position[0] = 16.46772;
-                  position[1] = 107.57914;
-                  markerPosition[0] = 16.46772;
-                  markerPosition[1] = 107.57914;
+                  position[0] = 0;
+                  position[1] = 0;
+                  markerPosition[0] = 0;
+                  markerPosition[1] = 0;
                 } else {
                   if (position[0] === 0 && position[1] === 0) {
                     position[0] = formData.location.coordinates.lat;
@@ -335,6 +334,16 @@ class PlaceEdit extends Component {
                     markerPosition[0] = formData.location.coordinates.lat;
                     markerPosition[1] = formData.location.coordinates.long;
                   }
+                }
+
+                if (position[0] === 0 && position[1] === 0) {
+                  position[0] = 16.46772;
+                  position[1] = 107.57914;
+                }
+
+                if (markerPosition[0] === 0 && markerPosition[1] === 0) {
+                  markerPosition[0] = 16.46772;
+                  markerPosition[1] = 107.57914;
                 }
 
                 return (
